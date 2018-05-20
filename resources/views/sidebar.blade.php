f85b08fee9e76adc80bb44fe50f007c5b3c69473
Jaume Moreno <morenocantoj@gmail.com>
2018-05-20 14:23:37 +0200

    Class active flama para el sidebar

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -7,19 +7,19 @@
         </div>
 
         <ul class="nav">
-            <li class="active">
+           <li {{request()->is('contratar') ? 'class=active' : ''}}>
                 <a href="{{ route("contratar") }}">
                     <i class="pe-7s-user"></i>
                     <p>Contratar personal</p>
                 </a>
             </li>
-            <li>
+             <li {{request()->is('subida') ? 'class=active' : ''}}>
                 <a href="{{ route("subida") }}">
                     <i class="pe-7s-copy-file"></i>
                     <p>Subida de archivos</p>
                 </a>
             </li>
-            <li>
+             <li {{request()->is('qasoftware') ? 'class=active' : ''}}>
                 <a href="{{ route("qasoftware") }}">
                     <i class="pe-7s-note2"></i>
                     <p>QA Software Apps</p>
@@ -28,19 +28,19 @@
             <li>
               <p class="logo">Paneles y placas</p>
             </li>
-            <li>
+             <li {{request()->is('manufactura') ? 'class=active' : ''}}>
                 <a href="{{ route("manufactura") }}">
                     <i class="pe-7s-tools"></i>
                     <p>Manufactura</p>
                 </a>
             </li>
-            <li>
+             <li {{request()->is('ecommerce') ? 'class=active' : ''}}>
                 <a href="{{ route("ecommerce") }}">
                     <i class="pe-7s-portfolio"></i>
                     <p>E-commerce</p>
                 </a>
             </li>
-            <li>
+             <li {{request()->is('programacion') ? 'class=active' : ''}}>
                 <a href="{{ route("programacion") }}">
                     <i class="pe-7s-timer"></i>
                     <p>Programación</p>
